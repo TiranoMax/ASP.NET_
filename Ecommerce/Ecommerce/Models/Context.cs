23fa0d2fd167e7b04d2dbe832c68665309096d24
TiranoMax <alexsandroarents@hotmail.com>
2018-07-30 21:40:18 -0300

    Criado A classe Context para conexão com o DB

---
+++ Ecommerce/Ecommerce/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+
+namespace Ecommerce_ASP_NET.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DbEcommerce") { }
+
+        //Mapear as classes que vao virar tabela no banco
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
