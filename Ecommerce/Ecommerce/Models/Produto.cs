dee2c48e9bc2eafb0db0ce63d7d5671033750179
TiranoMax <alexsandroarents@hotmail.com>
2018-07-30 21:50:18 -0300

    Arrumado pequeno erro de using na classe Model Produto

--- Ecommerce/Ecommerce/Models/Produto.cs
+++ Ecommerce/Ecommerce/Models/Produto.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Ecommerce_ASP_NET.Models
+namespace Ecommerce.Models
 {
     [Table("Produtos")]
     public class Produto
