6b006f464768a9a15ee7f7efec67be889806890d
TiranoMax <alexsandroarents@hotmail.com>
2018-07-30 21:51:26 -0300

    Criado A classe ProdutoController com seus comandos

--- Ecommerce/Ecommerce/Controllers/ProdutoController.cs
+++ Ecommerce/Ecommerce/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ecommerce.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,36 @@ namespace Ecommerce.Controllers
 {
     public class ProdutoController : Controller
     {
+        Context contexto = new Context();
         // GET: Produto
         public ActionResult Index()
         {
+            ViewBag.Data = DateTime.Now;
+            ViewBag.Produtos = contexto.Produtos.ToList();
             return View();
         }
+
+        public ActionResult CadastrarProduto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarProduto(string txtNome, string txtDescricao, string txtPreco, string txtCategoria)
+        {
+            Produto produto = new Models.Produto
+            {
+                Nome = txtNome,
+                Descricao = txtDescricao,
+                Preco = Convert.ToDouble(txtPreco),
+                Categoria = txtCategoria
+            };
+
+            contexto.Produtos.Add(produto);
+            contexto.SaveChanges();
+
+            return RedirectToAction("Index", "Produto");
+        }
+
     }
 }
