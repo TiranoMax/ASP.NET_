7939089b7e49b2f2526c890a8e1127b3d0c1f793
TiranoMax <alexsandroarents@hotmail.com>
2018-07-30 21:39:29 -0300

    Criado a classe Produto com seus respectivos atributos

---
+++ Ecommerce/Ecommerce/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce_ASP_NET.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public string Categoria { get; set; }
+        public string Imagem { get; set; }
+
+    }
+}
