516177bd08667844a9c470b3aab4774bb49f0a54
TiranoMax <alexsandroarents@hotmail.com>
2018-08-07 10:12:50 -0300

    Modificado a model para ter anotações importantes

--- Ecommerce/Ecommerce/Models/Produto.cs
+++ Ecommerce/Ecommerce/Models/Produto.cs
@@ -8,10 +8,24 @@ namespace Ecommerce.Models
     {
         [Key]
         public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage ="Campo Obrigatótio")]
+        [MaxLength(50, ErrorMessage="O campo deve conter no máximo 50 caractres")]
+        [Display(Name ="Nome do Produto")]
         public string Nome { get; set; }
+
+        [Display(Name = "Descrição do Produto")]
+        [DataType(DataType.MultilineText)]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatótio")]
+        [Display(Name = "Preço do Produto")]
+        //[Range]responsavel por limitar o tamanho de números
         public double Preco { get; set; }
+
+        [Display(Name = "Categoria do Produto")]
         public string Categoria { get; set; }
+
         public string Imagem { get; set; }
 
     }
