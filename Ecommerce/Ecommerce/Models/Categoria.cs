edd5ae461b934a7e83732639ce6bec2aa18ed07b
TiranoMax <alexsandroarents@hotmail.com>
2018-08-09 18:48:11 -0300

    Criado os atributos e as anotações da classe Categoria

--- Ecommerce/Ecommerce/Models/Categoria.cs
+++ Ecommerce/Ecommerce/Models/Categoria.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ecommerce.Models
 {
+    [Table("Categoria")]
     public class Categoria
     {
+        [Key]
+        public int CategoriaId { get; set; }
         
+        [Required(ErrorMessage = "Campo Obrigatótio")]
+        [MaxLength(50, ErrorMessage = "O campo deve conter no máximo 50 caractres")]
+        [Display(Name = "Nome da Categoria")]
+        public string NomeCategoria { get; set; }
+
+        [Display(Name = "Descrição da Categoria")]
+        [DataType(DataType.MultilineText)]
+        public string Descricao { get; set; }
+
 
     }
 }
