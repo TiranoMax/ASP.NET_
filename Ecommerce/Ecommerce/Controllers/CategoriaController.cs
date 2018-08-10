a8caba9a436089dd4e1858b217d33e0d06e39de9
TiranoMax <alexsandroarents@hotmail.com>
2018-08-09 22:19:29 -0300

    Criado os metodos no CategoriaController

--- Ecommerce/Ecommerce/Controllers/CategoriaController.cs
+++ Ecommerce/Ecommerce/Controllers/CategoriaController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Ecommerce.DAL;
+using Ecommerce.Models;
 using System.Web.Mvc;
 
 namespace Ecommerce.Controllers
@@ -11,7 +9,74 @@ namespace Ecommerce.Controllers
         // GET: Categoria
         public ActionResult Index()
         {
+            return View(CategoriaDAO.RetornarCategorias());
+        }
+
+        public ActionResult CadastrarCategoria()
+        {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult CadastrarCategoria(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                if (CategoriaDAO.CadastrarCategoria(categoria))
+                {
+                    return RedirectToAction("Index", "Produto"); //modificar
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Não é possivel adicionar uma categoria com um mesmo nome!");
+                    return View(categoria);
+                }
+            }
+            else
+            {
+                return View(categoria);
+            }
+        }
+
+        public ActionResult RemoverCategoria(int id)
+        {
+            CategoriaDAO.RemoverCategoria(id);
+
+            return RedirectToAction("Index", "Produto");
+        }
+
+
+        public ActionResult AlterarCategoria(int id)
+        {
+            return View(CategoriaDAO.BuscarCategoria(id));
+        }
+
+        [HttpPost]
+        public ActionResult AlterarCategoria(Categoria categoriaAlterado)
+        {
+
+            Categoria produtoOriginal = CategoriaDAO.BuscarCategoria(categoriaAlterado.CategoriaId);
+
+            produtoOriginal.NomeCategoria = categoriaAlterado.NomeCategoria;
+            produtoOriginal.Descricao = categoriaAlterado.Descricao;
+
+            if (ModelState.IsValid)
+            {
+                if (CategoriaDAO.AlterarCategoria(produtoOriginal))
+                {
+                    return RedirectToAction("Index", "Produto");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Não é possivel modificar, devido ja existir uma Categoria com o mesmo nome!");
+                    return View(produtoOriginal);
+                }
+            }
+            else
+            {
+                return View(produtoOriginal);
+            }
+        }
+
     }
 }
