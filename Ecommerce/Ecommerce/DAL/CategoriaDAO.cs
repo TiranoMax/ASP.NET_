800015baed058c8b91192b195c6f75212d6a6a7d
TiranoMax <alexsandroarents@hotmail.com>
2018-08-09 22:10:08 -0300

    Criados os metodos na classe CategoriaDao

--- Ecommerce/Ecommerce/DAL/CategoriaDAO.cs
+++ Ecommerce/Ecommerce/DAL/CategoriaDAO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Ecommerce.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,53 @@ namespace Ecommerce.DAL
 {
     public class CategoriaDAO
     {
+        private static Context contexto = new Context();
 
+        public static List<Categoria> RetornarCategorias()
+        {
+            return contexto.Categorias.ToList();
+        }
+
+        public static bool CadastrarCategoria(Categoria categoria)
+        {
+            if(BuscarCategoriaNome(categoria) == null)
+            {
+                contexto.Categorias.Add(categoria);
+                contexto.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static Categoria BuscarCategoriaNome(Categoria categoria)
+        {
+            return contexto.Categorias.FirstOrDefault(x => x.NomeCategoria.Equals(categoria.NomeCategoria));
+        }
+
+        public static void RemoverCategoria(int id)
+        {
+            contexto.Categorias.Remove(CategoriaDAO.BuscarCategoria(id));
+            contexto.SaveChanges();
+        }
+
+        public static Categoria BuscarCategoria(int id)
+        {
+            return contexto.Categorias.Find(id);
+        }
+
+        public static bool AlterarCategoria(Categoria categoria)
+        {
+            if (contexto.Categorias.FirstOrDefault(x => x.NomeCategoria.Equals(categoria.NomeCategoria) && x.CategoriaId != categoria.CategoriaId) == null)
+            {
+                contexto.Entry(categoria).State = EntityState.Modified;
+                contexto.SaveChanges();
+                return true;
+            }
+            return false;
+
+        }
     }
 }
