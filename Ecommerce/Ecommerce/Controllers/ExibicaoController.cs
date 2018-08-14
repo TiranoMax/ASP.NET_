0883c59dc4bf206454aea956e40adca087bb857e
TiranoMax <alexsandroarents@hotmail.com>
2018-08-14 11:20:32 -0300

    Pequeno erro arrumado

--- Ecommerce/Ecommerce/Controllers/ExibicaoController.cs
+++ Ecommerce/Ecommerce/Controllers/ExibicaoController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ecommerce.DAL;
+using Ecommerce.Models;
 
 namespace Ecommerce.Controllers
 {
     public class ExibicaoController : Controller
     {
+        private static Context contexto = SingletonContext.GetInstance();
+
         // GET: Exibicao
         public ActionResult Exibicao()
         {
+            return View(ProdutoDAO.RetornarProdutos());
+        }
+
+        public ActionResult Descricao(int id)
+        {
+            ViewBag.Mostrar = ProdutoDAO.BuscarProduto(id);
             return View();
         }
     }
