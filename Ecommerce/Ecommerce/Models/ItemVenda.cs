2bef15c0311bd560510f577791613573691c70f2
TiranoMax <alexsandroarents@hotmail.com>
2018-08-17 10:19:53 -0300

    Criado a model  ItemVenda com seus atributos

---
+++ Ecommerce/Ecommerce/Models/ItemVenda.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models
+{
+    [Table("ItemVenda")]
+    public class ItemVenda
+    {
+        [Key]
+        public int ItemVendaId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatótio")]
+        [Display(Name = "Nome do Produto")]
+        public Produto Produto { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatótio")]
+        [Display(Name = "Quantidade ")]
+        public int Qtde { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatótio")]
+        [Display(Name = "Preço")]
+        public double Preco { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatótio")]
+        [Display(Name = "Data de adicionamento")]
+        public DateTime Data { get; set; }
+
+    }
+}
