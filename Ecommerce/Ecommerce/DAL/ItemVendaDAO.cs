1e2a187f915551987b7bdff05e6da9407d319b20
TiranoMax <alexsandroarents@hotmail.com>
2018-08-17 10:52:54 -0300

    Criado a classe ItemVendaDAO

---
+++ Ecommerce/Ecommerce/DAL/ItemVendaDAO.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.DAL
+{
+    public class ItemVendaDAO
+    {
+        private static Context contexto = SingletonContext.GetInstance();
+    }
+}
