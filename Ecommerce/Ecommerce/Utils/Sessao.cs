963059cfff83f11a918bf143e9bbda836a71d2e7
TiranoMax <alexsandroarents@hotmail.com>
2018-08-21 10:20:10 -0300

    Criado uma sessão para o carrinho

---
+++ Ecommerce/Ecommerce/Utils/Sessao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Utils
+{
+    public class Sessao
+    {
+        private static string NOME_SESSAO = "CartId";
+
+        public static string RetornarCarrinhoId()
+        {
+            if(HttpContext.Current.Session[NOME_SESSAO] == null)
+            {
+                Guid guid = Guid.NewGuid();
+                HttpContext.Current.Session[NOME_SESSAO] = guid.ToString();
+            }
+            return HttpContext.Current.Session[NOME_SESSAO].ToString();
+        }
+    }
+}
