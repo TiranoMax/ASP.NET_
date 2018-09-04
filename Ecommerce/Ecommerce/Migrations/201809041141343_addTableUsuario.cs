5f817038d469df3210129a2bb379605f41e212f9
TiranoMax <alexsandroarents@hotmail.com>
2018-09-04 08:47:50 -0300

    Criado nova Migration

---
+++ Ecommerce/Ecommerce/Migrations/201809041141343_addTableUsuario.cs
@@ -0,0 +1,28 @@
+namespace Ecommerce.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addTableUsuario : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Usuarios",
+                c => new
+                    {
+                        UsuarioId = c.Int(nullable: false, identity: true),
+                        Nome = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        Senha = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.UsuarioId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Usuarios");
+        }
+    }
+}
