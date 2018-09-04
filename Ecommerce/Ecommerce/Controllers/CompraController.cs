e986a780dd783124267e984bda0e0d9be3664680
TiranoMax <alexsandroarents@hotmail.com>
2018-09-04 08:26:04 -0300

    Criado a Controller Compra

---
+++ Ecommerce/Ecommerce/Controllers/CompraController.cs
@@ -0,0 +1,31 @@
+﻿using Ecommerce.DAL;
+using Ecommerce.Models;
+using Ecommerce.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class CompraController : Controller
+    {
+        
+        // GET: Compra
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarCompra(Compra compra ) 
+        {
+            CompraDAO.CadastrarCompra(compra);
+
+            Sessao.NovaSessao();
+
+            return RedirectToAction( "Exibicao", "Exibicao", null);
+        }
+    }
+}
