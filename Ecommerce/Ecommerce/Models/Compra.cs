2027bc18d932739c8369c80d51aaa7cac32dad90
TiranoMax <alexsandroarents@hotmail.com>
2018-09-04 08:26:43 -0300

    Criado a model Compra

---
+++ Ecommerce/Ecommerce/Models/Compra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Ecommerce.Models
+{
+    [Table("Compra")]
+    public class Compra
+    {
+        [Key]
+        public int CompraId { get; set; }
+
+        public string NomeCliente { get; set; }
+
+        public string EnderecoCliente { get; set; }
+
+        public int TelCliente { get; set; }
+
+        public List<ItemVenda> ItemVendas { get; set; }
+
+        
+    }
+}
