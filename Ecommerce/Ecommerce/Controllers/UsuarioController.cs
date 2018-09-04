165e0922ef584b16429617766461b3adeba07589
TiranoMax <alexsandroarents@hotmail.com>
2018-09-04 09:29:59 -0300

    Arrumado um parametro

--- Ecommerce/Ecommerce/Controllers/UsuarioController.cs
+++ Ecommerce/Ecommerce/Controllers/UsuarioController.cs
@@ -14,30 +14,30 @@ namespace Ecommerce.Controllers
     public class UsuarioController : Controller
     {
         private Context db = new Context();
-       
+
         public ActionResult Index()
         {
             return View(UsuarioDAO.RetornarUsuarios());
         }
-        
+
 
         public ActionResult Create()
         {
             return View();
         }
 
-        
+
         [HttpPost]
-        public ActionResult Create([Bind(Include = "UsuarioId,Nome,Email,Senha")] Usuario usuario)
+        public ActionResult Create([Bind(Include = "UsuarioId,Nome,Email,Senha,ConfirmeSenha")] Usuario usuario)
         {
             if (ModelState.IsValid)
             {
                 if (UsuarioDAO.CadastrarUsuario(usuario))
                 {
-                    return RedirectToAction("Index","Usuario");
+                    return RedirectToAction("Index", "Usuario");
                 }
                 ModelState.AddModelError("", "Esse usuário já existe!");
-                return View(usuario);                
+                return View(usuario);
             }
             return View(usuario);
         }
