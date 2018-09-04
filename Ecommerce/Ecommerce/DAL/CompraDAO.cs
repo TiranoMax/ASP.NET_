15c019aad8b2435ef3b483d0a4adb8789cc5f074
TiranoMax <alexsandroarents@hotmail.com>
2018-09-04 08:27:13 -0300

    Criado a classe CompraDAL

---
+++ Ecommerce/Ecommerce/DAL/CompraDAO.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.DAL
+{
+    public class CompraDAO
+    {
+        private static Context contexto = SingletonContext.GetInstance();
+
+        public static void CadastrarCompra(Compra Compra)
+        {
+            
+                contexto.Compras.Add(Compra);
+                contexto.SaveChanges();
+
+        }
+
+
+    }
+}
