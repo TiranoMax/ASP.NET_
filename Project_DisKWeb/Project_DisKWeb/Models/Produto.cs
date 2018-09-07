b3b136b5b66c8ab11dc237600ccb427bbcd4a266
TiranoMax <alexsandroarents@hotmail.com>
2018-09-07 14:42:07 -0300

    Adicionado os respectivos Atributos na model Produto

--- Project_DisKWeb/Project_DisKWeb/Models/Produto.cs
+++ Project_DisKWeb/Project_DisKWeb/Models/Produto.cs
@@ -1,11 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Project_DisKWeb.Models
 {
+    [Table("Produto")]
     public class Produto
     {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Categoria { get; set; }
+
+        public DateTime Ano_Lancamento { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int QTDE_Estoque { get; set; }
+
+        public double Preco_Venda { get; set; }
+
+        public double Preco_Aluguel { get; set; }
+
+        public string Img { get; set; }
     }
 }
