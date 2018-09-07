d59879bae60b75424e50a66d93c71bfc62383687
TiranoMax <alexsandroarents@hotmail.com>
2018-09-07 13:56:40 -0300

    Criado A contexto para o banco

---
+++ DisKWeb/DisKWeb/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DisKWeb.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DbDisKWeb") { }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
