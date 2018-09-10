307b33dfa5d9c576b2f2670bfd283643c9bd0f8e
TiranoMax <alexsandroarents@hotmail.com>
2018-09-10 17:17:48 -0300

    Arrumado o produtoController para ficar na padronizacao mvc

--- Project_DisKWeb/Project_DisKWeb/Controllers/ProdutoController.cs
+++ Project_DisKWeb/Project_DisKWeb/Controllers/ProdutoController.cs
@@ -6,122 +6,76 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Project_DisKWeb.DAL;
 using Project_DisKWeb.Models;
 
 namespace Project_DisKWeb.Controllers
 {
     public class ProdutoController : Controller
     {
-        private Context db = new Context();
-
-        // GET: Produto
+       
         public ActionResult Index()
         {
-            return View(db.Produtos.ToList());
-        }
-
-        // GET: Produto/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Produto produto = db.Produtos.Find(id);
-            if (produto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(produto);
+            return View(ProdutoDAO.ListProduto());
         }
 
         // GET: Produto/Create
-        public ActionResult Create()
+        public ActionResult CadProduto()
         {
             return View();
         }
 
-        // POST: Produto/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProdutoId,Nome,Categoria,Ano_Lancamento,Autor,Descricao,QTDE_Estoque,Preco_Venda,Preco_Aluguel,Img")] Produto produto)
+      
+        [HttpPost]        
+        public ActionResult CadProduto([Bind(Include = "ProdutoId,Nome,Categoria,Ano_Lancamento,Autor,Descricao,QTDE_Estoque,Preco_Venda,Preco_Aluguel,Img")] Produto produto)
         {
             if (ModelState.IsValid)
             {
-                db.Produtos.Add(produto);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                ProdutoDAO.CadProduto(produto);
+                
+                return RedirectToAction("Index", "Produto"); 
             }
-
-            return View(produto);
+            else
+            {
+                return View(produto);
+            }
         }
 
-        // GET: Produto/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Produto produto = db.Produtos.Find(id);
-            if (produto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(produto);
-        }
 
-        // POST: Produto/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProdutoId,Nome,Categoria,Ano_Lancamento,Autor,Descricao,QTDE_Estoque,Preco_Venda,Preco_Aluguel,Img")] Produto produto)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(produto).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(produto);
-        }
 
-        // GET: Produto/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Produto produto = db.Produtos.Find(id);
-            if (produto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(produto);
-        }
 
-        // POST: Produto/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Produto produto = db.Produtos.Find(id);
-            db.Produtos.Remove(produto);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// GET: Produto/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+            
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+       
+        //[HttpPost]
+        
+        //public ActionResult Edit([Bind(Include = "ProdutoId,Nome,Categoria,Ano_Lancamento,Autor,Descricao,QTDE_Estoque,Preco_Venda,Preco_Aluguel,Img")] Produto produto)
+        //{
+            
+        //}
+
+        //// GET: Produto/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        
+        //    Produto produto = db.Produtos.Find(id);
+          
+        //}
+
+        //// POST: Produto/Delete/5
+        //[HttpPost]        
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Produto produto = db.Produtos.Find(id);
+        //    db.Produtos.Remove(produto);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+       
     }
 }
