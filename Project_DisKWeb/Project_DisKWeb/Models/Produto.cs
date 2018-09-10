06e2e52ecb3c26e1f64b28570845352f47856dfd
TiranoMax <alexsandroarents@hotmail.com>
2018-09-10 16:02:13 -0300

    Addicionado anotações na classe model produto - DisKWeb

--- Project_DisKWeb/Project_DisKWeb/Models/Produto.cs
+++ Project_DisKWeb/Project_DisKWeb/Models/Produto.cs
@@ -10,20 +10,37 @@ namespace Project_DisKWeb.Models
         [Key]
         public int ProdutoId { get; set; }
 
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        [MaxLength(50, ErrorMessage = "O campo deve conter no máximo 50 caracteres")]
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        [MaxLength(50, ErrorMessage = "O campo deve conter no máximo 50 caracteres")]
+        [Display(Name = "Categoria")]
         public string Categoria { get; set; }
 
+        [Display(Name = "Ano de Lançamento")]
         public DateTime Ano_Lancamento { get; set; }
 
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        [MaxLength(50, ErrorMessage = "O campo deve conter no máximo 50 caracteres")]
+        [Display(Name = "Autor / Artista")]
         public string Autor { get; set; }
 
+        [Display(Name = "Descrição")]
+        [DataType(DataType.MultilineText)]
         public string Descricao { get; set; }
 
+        [Display(Name = "Quantidade em estoque")]
         public int QTDE_Estoque { get; set; }
 
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [Display(Name = "Preço de venda")]
         public double Preco_Venda { get; set; }
 
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [Display(Name = "Preço de Aluguel")]
         public double Preco_Aluguel { get; set; }
 
         public string Img { get; set; }
