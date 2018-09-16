0c9974990ad29728541dee1a785d9fa151747f63
TiranoMax <alexsandroarents@hotmail.com>
2018-09-16 03:14:06 -0300

    Criado a pasta Utils e a classe sessao - DisKWeb

---
+++ Project_DisKWeb/Project_DisKWeb/Utils/Sessao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Utils
+{
+    public class Sessao
+    {
+        private static string NOME_SESSAO = "";
+
+        public static string RetornarCarrinhoId()
+        {
+            if (HttpContext.Current.Session[NOME_SESSAO] == null)
+            {
+                Guid guid = Guid.NewGuid();
+                HttpContext.Current.Session[NOME_SESSAO] = guid.ToString();
+            }
+            return HttpContext.Current.Session[NOME_SESSAO].ToString();
+        }
+
+        public static void NovaSessao()
+        {
+            Guid guid = Guid.NewGuid();
+            HttpContext.Current.Session[NOME_SESSAO] = guid.ToString();
+        }
+
+    }
+}
