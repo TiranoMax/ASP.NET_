a990e82d5bc4fc826a25de69508d9520694bf82b
TiranoMax <alexsandroarents@hotmail.com>
2018-09-18 23:12:30 -0300

    Criado o O usuarioDAO - DisKWeb

---
+++ Project_DisKWeb/Project_DisKWeb/DAL/UsuarioDAO.cs
@@ -0,0 +1,34 @@
+﻿using Project_DisKWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_DisKWeb.DAL
+{
+    public class UsuarioDAO
+    {
+        private static Context ctx = Singleton.GetInstance();
+        public static bool CadUser (Usuario usuario)
+        {
+            if (BuscarUsuarioPorEmail(usuario) == null)
+            {
+                ctx.Usuarios.Add(usuario);
+                ctx.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public static Usuario BuscarUsuarioPorEmail(Usuario usuario)
+        {
+            return ctx.Usuarios.FirstOrDefault(x => x.Email.Equals(usuario.Email));
+        }
+
+        public static Usuario BucarUsuarioPorEmailESenha(Usuario usuario)
+        {
+            return ctx.Usuarios.FirstOrDefault(x => x.Email.Equals(usuario.Email) && x.Senha.Equals(usuario.Senha));
+        }
+
+    }
+}
